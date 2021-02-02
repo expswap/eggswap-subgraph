ca79ecabfd4115a4f8e15d121e3be3fe853e82a1
iquidus <luke@iquidus.io>
2021-02-02 20:40:57 +0100

    shinobi

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -7,7 +7,7 @@ import { User, Bundle, Token, LiquidityPosition, LiquidityPositionSnapshot, Pair
 import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xbA831e62ac14d8500cEF0367b14F383d7b1B1b0A'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
---
+++ src/mappings/oracle.ts
@@ -0,0 +1,40 @@
+/* eslint-disable prefer-const */
+import { PriceStore } from '../types/schema'
+import { Oracle } from '../types/Oracle/Oracle'
+import { Address, EthereumBlock, json, JSONValueKind } from '@graphprotocol/graph-ts'
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts/index'
+import { exponentToBigDecimal } from './helpers'
+import { ZERO_BD } from './helpers'
+
+const ORACLE_ADDRESS = '0xaefF2F7644f1C615aDb309513c4CB564F44Bb68F' // created block 1322544
+
+export function getEthPriceInUSD(): BigDecimal {
+  // fetch price from market oracle
+  let oracle = PriceStore.load(ORACLE_ADDRESS)
+  if (oracle == null) {
+    return ZERO_BD
+  } else {
+    return oracle.usd
+  }
+}
+
+export function priceUpdate(block: EthereumBlock): void {
+  let oracle = PriceStore.load(ORACLE_ADDRESS)
+  if (oracle == null) {
+    oracle = new PriceStore(ORACLE_ADDRESS)
+    oracle.usd =  ZERO_BD
+  } else {
+    let contract = Oracle.bind(Address.fromString(ORACLE_ADDRESS))
+    if (contract == null) {
+      oracle.usd =  ZERO_BD
+    } else {
+      let data: BigInt = contract.getPriceUsd()
+      if (data == null) {
+        oracle.usd =  ZERO_BD
+      } else {
+        oracle.usd = data.toBigDecimal().div(exponentToBigDecimal(BigInt.fromI32(6)))
+      }
+    }
+  }
+  oracle.save()
+}
